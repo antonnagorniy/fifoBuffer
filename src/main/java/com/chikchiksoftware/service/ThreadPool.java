0e6e15d7592194b77ee0af8758db52e977a7eefc
AntonNagornyi <anton.nagornyi@ugasoft.com>
2018-02-26 12:44:57 +0300

    [+] ThreadPool

---
+++ src/main/java/com/chikchiksoftware/service/ThreadPool.java
@@ -0,0 +1,46 @@
+package com.chikchiksoftware.service;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.Executor;
+
+/**
+ * Created by
+ *
+ * @authors Anton Nagornyi
+ * on 26.02.2018.
+ */
+public class ThreadPool implements Executor {
+    private final Queue<Runnable> workQueue = new ConcurrentLinkedQueue<>();
+    private volatile boolean isRunning = true;
+
+    public ThreadPool(int nThreads) {
+        for (int i = 0; i < nThreads; i++) {
+            new Thread(new TaskWorker()).start();
+        }
+    }
+
+    @Override
+    public void execute(Runnable command) {
+        if (isRunning) {
+            workQueue.offer(command);
+        }
+    }
+
+    public void shutdown() {
+        isRunning = false;
+    }
+
+    private final class TaskWorker implements Runnable {
+
+        @Override
+        public void run() {
+            while (isRunning) {
+                Runnable nextTask = workQueue.poll();
+                if (nextTask != null) {
+                    nextTask.run();
+                }
+            }
+        }
+    }
+}

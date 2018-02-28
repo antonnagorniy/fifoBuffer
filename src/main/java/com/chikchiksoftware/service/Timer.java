049e97569df00f24ceece7f2d69e446a67f2734d
AntonNagornyi <anton.nagornyi@ugasoft.com>
2018-03-01 02:21:54 +0300

    [~] Timer

--- src/main/java/com/chikchiksoftware/service/Timer.java
+++ src/main/java/com/chikchiksoftware/service/Timer.java
@@ -2,8 +2,6 @@ package com.chikchiksoftware.service;
 
 import com.chikchiksoftware.FifoFileBuffer;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * Created by
  *
@@ -35,31 +33,11 @@ public class Timer implements Runnable {
             System.out.println("==========================================");
             System.out.println("Produced: " + buffer.getProducedItems());
             System.out.println("Consumed: " + buffer.getConsumedItems());
-            System.out.println("Working time: " + millisToDHMS(end - start));
-            System.out.println("Producers remaining time to work: " + millisToDHMS((start + workingTime) - System.currentTimeMillis()));
+            System.out.println("Working time: " + TimeConversionService.millisToDHMS(end - start));
+            System.out.println("Producers remaining time to work: " + TimeConversionService.millisToDHMS((start + workingTime) - System.currentTimeMillis()));
             System.out.println("==========================================");
         }
     }
 
-    public String millisToDHMS(long duration) {
-        String result;
 
-        if(duration < 0) {
-            return "00:00:00";
-        }
-        long days  = TimeUnit.MILLISECONDS.toDays(duration);
-        long hours = TimeUnit.MILLISECONDS.toHours(duration)
-                - TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(duration));
-        long minutes = TimeUnit.MILLISECONDS.toMinutes(duration)
-                - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(duration));
-        long seconds = TimeUnit.MILLISECONDS.toSeconds(duration)
-                - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration));
-        if (days == 0) {
-            result = String.format("%02d:%02d:%02d", hours, minutes, seconds);
-        }
-        else {
-            result = String.format("%dd%02d:%02d:%02d", days, hours, minutes, seconds);
-        }
-        return result;
-    }
 }

20b31dbc78e07c5e22048db1960c771254e85422
Anton Nagorniy <ad.nagornyj@gmail.com>
2018-03-10 01:37:38 +0300

    [~] Main: Consumers & Producers change to daemons

--- src/main/java/com/chikchiksoftware/Main.java
+++ src/main/java/com/chikchiksoftware/Main.java
@@ -34,23 +34,24 @@ public class Main {
         FifoFileBuffer<Timestamp> buffer = new FifoFileBuffer<>();
         Timer serviceTimer = new Timer(buffer, start, interactions.getProducerTimeToWork());
 
-        producers.setDaemon(true);
-
         for(int i = 0; i < interactions.getProducersCount(); i++) {
-            new Thread(producers, new Producer(
+            Thread thread = new Thread(producers, new Producer(
                     buffer,
                     interactions.getFrequency(),
                     interactions.getProducerTimeToWork())
-            ).start();
+            );
+            thread.setDaemon(true);
+            thread.start();
         }
 
         Thread timerDaemon = new Thread(serviceTimer);
         timerDaemon.setDaemon(true);
         timerDaemon.start();
 
-        consumers.setDaemon(true);
         for(int i = 0; i < interactions.getConsumersCount(); i++) {
-            new Thread(consumers, new Consumer(buffer)).start();
+            Thread thread = new Thread(consumers, new Consumer(buffer));
+            thread.setDaemon(true);
+            thread.start();
         }
 
 
@@ -59,7 +60,7 @@ public class Main {
         fileCleaningDaemon.start();
 
         Runnable statistics = () -> {
-            while(producers.activeCount() > 0 || consumers.activeCount() > 0) {
+            while(producers.activeCount() > 0 || !buffer.isEmpty()) {
                 try {
                     Thread.sleep(500);
                 }catch(InterruptedException e) {
@@ -75,9 +76,11 @@ public class Main {
             System.out.println("Time elapsed: " + TimeConversionService.millisToDHMS(end - start));
             System.out.println("Data file length: " + (Math.round(buffer.getDataFileLength() / 1024)) + " Kb");
             System.out.println("===================================");
+            buffer.finish();
         };
 
         Thread finalStatisticsService = new Thread(statistics);
         finalStatisticsService.start();
+
     }
 }

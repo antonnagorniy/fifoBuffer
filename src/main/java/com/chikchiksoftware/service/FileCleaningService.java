76a0acca53865eb79bdfea7fdcf6b91d333854b4
Anton Nagorniy <ad.nagornyj@gmail.com>
2018-03-10 15:07:39 +0300

    [~] FileCleaningService: fix run()

--- src/main/java/com/chikchiksoftware/service/FileCleaningService.java
+++ src/main/java/com/chikchiksoftware/service/FileCleaningService.java
@@ -21,20 +21,25 @@ public class FileCleaningService implements Runnable {
     public void run() {
         while(true) {
             try {
-                if(buffer.getDataFileLength() < buffer.getDataFileMaxLength()) {
-                    Thread.sleep(500);
-                }else {
+                Thread.sleep(1000);
+            }catch(InterruptedException e) {
+                e.printStackTrace();
+            }
+            while(buffer.getDataFileLength() > buffer.getDataFileMaxLength()) {
+                try {
                     System.out.println("File current size: " + (buffer.getDataFileLength() / 1024) + "Kb");
                     System.out.println("File cleaning in progress...");
                     buffer.fileDump();
                     System.out.println("File cleaned.");
                     System.out.println("File new size: " + (buffer.getDataFileLength() / 1024) + "Kb");
-                    Thread.sleep(500);
+                }catch(Exception e) {
+                    System.err.println("Cleaning Service thread failed:");
+                    e.printStackTrace();
                 }
-            }catch(Exception e) {
-                System.err.println("Cleaning Service thread failed:");
-                e.printStackTrace();
             }
+
+
         }
+
     }
 }

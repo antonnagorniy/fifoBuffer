4cf339017b1cea528cdff959161abd68952cfc01
Anton Nagorniy <ad.nagornyj@gmail.com>
2018-03-10 16:34:37 +0300

    [~] FileCleaningService: fixed run()

--- src/main/java/com/chikchiksoftware/service/FileCleaningService.java
+++ src/main/java/com/chikchiksoftware/service/FileCleaningService.java
@@ -26,16 +26,11 @@ public class FileCleaningService implements Runnable {
                 e.printStackTrace();
             }
             while(buffer.getDataFileLength() > buffer.getDataFileMaxLength()) {
-                try {
-                    System.out.println("File current size: " + (buffer.getDataFileLength() / 1024) + "Kb");
-                    System.out.println("File cleaning in progress...");
-                    buffer.fileDump();
-                    System.out.println("File cleaned.");
-                    System.out.println("File new size: " + (buffer.getDataFileLength() / 1024) + "Kb");
-                }catch(Exception e) {
-                    System.err.println("Cleaning Service thread failed:");
-                    e.printStackTrace();
-                }
+                System.out.println("File current size: " + (buffer.getDataFileLength() / 1024) + "Kb");
+                System.out.println("File cleaning in progress...");
+                buffer.fileDump();
+                System.out.println("File cleaned.");
+                System.out.println("File new size: " + (buffer.getDataFileLength() / 1024) + "Kb");
             }
 
 

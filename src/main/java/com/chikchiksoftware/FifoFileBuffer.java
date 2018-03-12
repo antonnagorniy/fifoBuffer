1f781b10f10ecc2e8c26253befb05a47253eac47
Anton Nagorniy <ad.nagornyj@gmail.com>
2018-03-12 21:55:34 +0300

    [~] FifoFileBuffer: fix implementation

--- src/main/java/com/chikchiksoftware/FifoFileBuffer.java
+++ src/main/java/com/chikchiksoftware/FifoFileBuffer.java
@@ -2,9 +2,7 @@ package com.chikchiksoftware;
 
 import java.io.*;
 import java.sql.Timestamp;
-import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 /**
  * Created by
@@ -14,17 +12,14 @@ import java.util.concurrent.CopyOnWriteArrayList;
  */
 
 public class FifoFileBuffer<T> implements java.io.Serializable {
+
     private final Object lock = new Object();
     private File dataFile = new File(new Timestamp(System.currentTimeMillis()).getTime() + ".dta");
-    private final long dataFileMaxLength = 104857600/*20480*//*1024*/;
+    private final long dataFileMaxLength;
     private long count;
     private long offset;
-
-    /**
-     * Used as data store while Data file is in
-     * cleaning process
-     */
-    private final List<T> cacheList = new CopyOnWriteArrayList<>();
+    private long produced;
+    private long consumed;
 
     private ObjectOutputStream objectOutputStream = null;
     private ObjectInputStream objectInputStream = null;
@@ -33,8 +28,9 @@ public class FifoFileBuffer<T> implements java.io.Serializable {
      * Creates an {@code FifoFileBuffer} with default params
      *
      */
-    public FifoFileBuffer() {
-        dataFile.deleteOnExit();
+    public FifoFileBuffer(long bufferBytesLength) {
+        this.dataFileMaxLength = bufferBytesLength;
+        this.dataFile.deleteOnExit();
     }
 
     /**
@@ -46,23 +42,29 @@ public class FifoFileBuffer<T> implements java.io.Serializable {
         synchronized(lock) {
             if(data != null) {
                 if(isDataFileFull()) {
-                    cacheList.add(data);
-                    lock.notifyAll();
-                }else {
                     try {
-                        if(objectOutputStream == null) {
-                            objectOutputStream = new ObjectOutputStream(new FileOutputStream(dataFile, true));
-                        }
-                        objectOutputStream.writeObject(data);
-                        objectOutputStream.flush();
-                        count++;
-                    }catch(IOException e) {
-                        System.err.println("Error writing to file " + e.getCause());
-                    }catch(NoSuchElementException e) {
-                        System.err.println("Invalid input: " + e.getCause());
-                    }finally {
-                        lock.notifyAll();
+                        lock.wait();
+                    }catch(InterruptedException e) {
+                        e.printStackTrace();
                     }
+                }
+                try {
+                    if(!dataFile.exists()) {
+                        createNewEmptyDataFile();
+                    }
+                    if(objectOutputStream == null) {
+                        objectOutputStream = new ObjectOutputStream(new FileOutputStream(dataFile, true));
+                    }
+                    objectOutputStream.writeObject(data);
+                    objectOutputStream.flush();
+                    count++;
+                    produced++;
+                }catch(IOException e) {
+                    System.err.println("Error writing to file " + e.getCause());
+                }catch(NoSuchElementException e) {
+                    System.err.println("Invalid input: " + e.getCause());
+                }finally {
+                    lock.notifyAll();
                 }
             }else {
                 lock.notifyAll();
@@ -76,12 +78,15 @@ public class FifoFileBuffer<T> implements java.io.Serializable {
      * Takes element from the head of this buffer
      *
      * @return Data.toString
-     * @throws EOFException if element is null
+     * @throws EOFException if file is empty
      */
     public T take() throws IOException{
         synchronized(lock) {
             try {
                 while(isEmpty()) {
+                    if(isDataFileFull()) {
+                        finish();
+                    }
                     lock.wait();
                 }
             }catch(InterruptedException e) {
@@ -94,14 +99,13 @@ public class FifoFileBuffer<T> implements java.io.Serializable {
                 if(objectInputStream == null) {
                     objectInputStream = new ObjectInputStream(new FileInputStream(dataFile));
                 }
-
                 currentFirstElement = (T) objectInputStream.readObject();
-
+                offset++;
+                consumed++;
             }catch(ClassNotFoundException e) {
                 System.err.println("Object deserialization failed: " + e.getCause());
             }
 
-            offset++;
             lock.notifyAll();
             return currentFirstElement;
         }
@@ -114,6 +118,79 @@ public class FifoFileBuffer<T> implements java.io.Serializable {
      */
     public boolean isEmpty() {
         return (count == offset);
+    }
+
+    /**
+     * Get difference between count of added elements and current offset
+     *
+     * @return long
+     */
+    public long getSize() {
+        return (count - offset);
+    }
+
+    /**
+     * Get count of added elements
+     *
+     * @return long
+     */
+    public long getCount() {
+        return count;
+    }
+
+    /**
+     * Get count of taken elements
+     *
+     * @return long
+     */
+    public long getOffset() {
+        return offset;
+    }
+
+    /**
+     * Get length of data file
+     *
+     * @return long
+     */
+    public long getDataFileLength() {
+        return dataFile.length();
+    }
+
+    /**
+     * Creates new empty data file
+     *
+     */
+    private void createNewEmptyDataFile() {
+        dataFile = new File(new Timestamp(System.currentTimeMillis()).getTime() + ".dta");
+        dataFile.deleteOnExit();
+    }
+
+    /**
+     * Checks if data file reached length limit
+     *
+     * @return boolean
+     */
+    private boolean isDataFileFull() {
+        return (dataFile.length() >= dataFileMaxLength);
+    }
+
+    /**
+     * For testing purposes
+     *
+     *
+     * @return count of all added elements
+     */
+    public long getProduced() {
+        return produced;
+    }
+
+    /**
+     * For testing purposes
+     *
+     * @return count of all taken elements
+     */
+    public long getConsumed() {
+        return consumed;
     }
 
     /**
@@ -134,99 +211,11 @@ public class FifoFileBuffer<T> implements java.io.Serializable {
         }catch(IOException e) {
             System.err.println("Error closing streams " + e.getCause());
         }finally {
-            dataFile.delete();
+
         }
-    }
-
-    /**
-     * Get difference between count of added elements and current offset
-     *
-     * @return long
-     */
-    public long getSize() {
-        return (count - offset);
-    }
-
-    /**
-     * Get count of added elements
-     *
-     * @return long
-     */
-    public long getAllAddedItemsCount() {
-        return count;
-    }
-
-    /**
-     * Get count of taken elements
-     *
-     * @return long
-     */
-    public long getAllTakenItemsCount() {
-        return offset;
-    }
-
-    /**
-     * Get length of data file
-     *
-     * @return long
-     */
-    public long getDataFileLength() {
-        return dataFile.length();
-    }
-
-    /**
-     * Get data file length limitation
-     *
-     * @return long
-     */
-    public long getDataFileMaxLength() {
-        return dataFileMaxLength;
-    }
-
-    /**
-     * Creates new empty data file
-     *
-     */
-    private void createNewEmptyDataFile() {
-        dataFile = new File(new Timestamp(System.currentTimeMillis()).getTime() + ".dta");
-
-    }
-
-    /**
-     * Checks if data file reached length limit
-     *
-     * @return
-     */
-    public boolean isDataFileFull() {
-        return (dataFile.length() >= dataFileMaxLength);
-    }
-
-    /**
-     * Dumps data file
-     *
-     */
-    public void fileDump() {
-        synchronized(lock) {
-            if(isDataFileFull() && isEmpty()) {
-                finish();
-                createNewEmptyDataFile();
-
-                for(T object : cacheList) {
-                    put(object);
-                }
-
-                cacheList.clear();
-                System.out.println("File cleaned.");
-            }else {
-                try {
-                    lock.wait();
-                }catch(InterruptedException e) {
-                    System.err.print("File dump failed: ");
-                    e.printStackTrace();
-                }
-            }
-
-            lock.notifyAll();
-        }
+        dataFile.delete();
+        count = 0;
+        offset = 0;
+        System.out.println("File cleaned.");
     }
 }
